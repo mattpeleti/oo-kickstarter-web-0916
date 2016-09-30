5401dcf52c4dae5f7a6ca7a971df55b1882dd9d3
github username <github email address>
2016-09-30 15:25:24 -0400

    done

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+require "pry"
+class Backer
+  attr_reader :name, :backed_projects
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    project.add_backer(self)
+    @backed_projects << project
+  end
+
+
+
+
+
+end
